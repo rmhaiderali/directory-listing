f8acb70883d31d9bac5ee22f0bc748f281660e31
Haider Ali <rmha64@gmail.com>
2024-04-01 17:44:27 +0500

    Enhanced path support

--- src/index.php
+++ src/index.php
@@ -29,6 +29,10 @@
             margin: 1rem auto 1rem 0rem;
         }
 
+        video:fullscreen {
+            border: none;
+        }
+
         p {
             margin: 5px 0;
         }
@@ -88,8 +92,6 @@
 <body>
     <?php
 
-    error_reporting(0);
-
     $exts = [
         "video" => [".mp4", ".mkv", ".mov", ".webm"],
         "audio" => [".mp3", ".m4a", ".wav", ".ogg", ".aac"],
@@ -103,29 +105,46 @@
         else return round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . $sizes[$i];
     }
 
-    $homeDirectory = ".";
-    $homeDirectoryLength = strlen($homeDirectory);
-    $currentDir = $homeDirectory;
-
-    if ($path = $_REQUEST["path"]) {
-        if (substr($path, -1) === "/") $path = substr($path, 0, -1);
-
-        $currentDir = $homeDirectory . $path;
-        $showDir = $path;
+    // https://stackoverflow.com/questions/1091107/how-to-join-filesystem-path-strings-in-php
+    function join_paths()
+    {
+        $paths = array();
+        foreach (func_get_args() as $arg) {
+            if ($arg !== "") $paths[] = $arg;
+        }
+        return preg_replace("#/+#", "/", join("/", $paths));
     }
+
+    function nth_last_index_of($haystack, $needle, $nth)
+    {
+        $len = strlen($haystack);
+        $pos = $len;
+        for ($i = 0; $i < $nth; $i++) {
+            $pos = strrpos($haystack, $needle, $pos - $len - 1);
+            if ($pos === false) return false;
+        }
+        return $pos;
+    }
+
+    $homeDirectory = "./";
 
     $files = [];
     $folders = [];
-    $media = $_GET["media"];
-    $dark = $_GET["dark"];
 
-    if ($media !== "1") $media = "0";
-    if ($dark !== "0") $dark = "1";
+    $media = isset($_GET["media"]) ? $_GET["media"] : "0";
+    $dark = isset($_GET["dark"]) ? $_GET["dark"] : "1";
+    $path = isset($_REQUEST["path"]) ? $_REQUEST["path"] : "/";
+
+    $externalDir = join_paths("/", $path, "/");
+    $internalDir = join_paths($homeDirectory, $externalDir);
+
+    if ($path !== $externalDir)
+        header("Location: ?dark=$dark&media=$media&path=$externalDir", true, 301);
 
     if ($dark !== "0")
         echo "<script>document.documentElement.classList.add('dark')</script>";
 
-    if (!($openedDir = opendir($currentDir)))
+    if (!is_dir($internalDir))
         die("<b class'break-all'>Directory does not exist.<b>");
 
     if ($_FILES) {
@@ -137,8 +156,8 @@
 
             if (!$tmpFileName) break;
 
-            echo "<p>Saved $fileName to $showDir</p>";
-            move_uploaded_file($tmpFileName, "$currentDir/$fileName");
+            echo "<p>Saved $fileName to $externalDir</p>";
+            move_uploaded_file($tmpFileName, "$internalDir/$fileName");
         }
     }
 
@@ -146,24 +165,26 @@
     echo "<input type='file' name='files[]' multiple>";
     echo "<input type='submit' value='Upload'>";
     echo "</form>";
-    echo "<b class='break-all'>Index of $showDir/ </b>";
+    echo "<b class='break-all'>Index of $externalDir</b>";
 
-    if ($currentDir !== $homeDirectory) {
+    if ($internalDir !== join_paths($homeDirectory, "/")) {
 
-        $lastSlashIndex = strrpos($showDir, "/");
-        $parentDir = substr($showDir, 0, $lastSlashIndex);
+        $secondLastSlashIndex = nth_last_index_of($externalDir, "/", 2);
+        $parentDir = substr($externalDir, 0, $secondLastSlashIndex);
 
         echo "<p>⤶ <a class='folders break-all' href='?dark=$dark&media=$media&path=$parentDir/'>Parent Directory</a></p>";
     }
+
+    $openedDir = opendir($internalDir);
 
     while (($item = readdir($openedDir)) !== false) {
 
         if ($item == "." or $item == "..") continue;
 
-        $path = "$currentDir/$item";
+        $itemPath = "$internalDir$item";
 
-        if (is_dir($path)) {
-            $link = "<a class='folders break-all' href='?dark=$dark&media=$media&path=$showDir/$item/'>$item</a>";
+        if (is_dir($itemPath)) {
+            $link = "<a class='folders break-all' href='?dark=$dark&media=$media&path=$externalDir$item/'>$item</a>";
 
             array_push($folders, $link);
         } else {
@@ -172,14 +193,14 @@
 
                 $ext = strtolower(strrchr($item, "."));
 
-                if (in_array($ext, $exts["image"])) $view = "<img src='$path'>";
-                else if (in_array($ext, $exts["audio"])) $view = "<audio controls><source src='$path'></audio>";
-                else if (in_array($ext, $exts["video"])) $view = "<video controls><source src='$path'></video>";
+                if (in_array($ext, $exts["image"])) $view = "<img src='$itemPath'>";
+                else if (in_array($ext, $exts["audio"])) $view = "<audio controls><source src='$itemPath'></audio>";
+                else if (in_array($ext, $exts["video"])) $view = "<video controls><source src='$itemPath'></video>";
             }
 
-            $link = "<a class='files break-all' href='$path'>$item</a>";
-            $size = "<span class='whitespace-pre'> ~ " . format_size(filesize($path)) . "</span>";
-            array_push($files, $link . $size . $view);
+            $link = "<a class='files break-all' href='$itemPath'>$item</a>";
+            $size = "<span class='whitespace-pre'> ~ " . format_size(filesize($itemPath)) . "</span>";
+            array_push($files, $link . $size . (isset($view) ? $view : ""));
 
             unset($view);
         }
